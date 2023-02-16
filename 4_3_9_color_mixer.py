bf61427807f1eb277bb18e6ec1f83515fef392fd
Dagvello <dagvello@ukr.net>
2023-02-17 01:21:17 +0200

    Рабочее состояние

--- 4_3_9_color_mixer.py
+++ 4_3_9_color_mixer.py
@@ -1,14 +1,17 @@
-colors = {'красный: 1', 'синий: 2', 'желтый: 3'}
-mix_color = {'3: фиолетовый', '4: оранжевый', '5: зеленый'}
+colors = {'красный': 1, 'синий': 3, 'желтый': 6}
+mix_colors = {2: 'красный', 4: 'фиолетовый', 6: 'синий', 7: 'оранжевый', 9: 'зеленый', 12: 'желтый'}
 
 
 def mix_color(color_1: str, color_2: str) -> str:
     """Смешение цветов"""
-    if color_1 or color_2 not in colors:
-        return print('ошибка цвета')
+
+    if color_1 in 'красныйсинийжелтый' and color_2 in 'красныйсинийжелтый':
+        return print(mix_colors[colors[color_1] + colors[color_2]])
     else:
-        return print(mix_color[colors[color_1] + colors[color_2]])
+        return print('ошибка цвета')
 
 
 if __name__ == '__main__':
     color_1, color_2 = input(), input()
+    mix_color(color_1, color_2)
+
