d086ddb27a04ab9e8010bfd66de6e193bf69d932
Dagvello <dagvello@ukr.net>
2023-02-17 01:29:36 +0200

    Исправление условного оператора

--- 4_3_9_color_mixer.py
+++ 4_3_9_color_mixer.py
@@ -5,7 +5,7 @@ mix_colors = {2: 'красный', 4: 'фиолетовый', 6: 'синий', 7
 def mix_color(color_1: str, color_2: str) -> str:
     """Смешение цветов"""
 
-    if color_1 in 'красныйсинийжелтый' and color_2 in 'красныйсинийжелтый':
+    if color_1 and color_2 in 'красныйсинийжелтый':
         return print(mix_colors[colors[color_1] + colors[color_2]])
     else:
         return print('ошибка цвета')
@@ -14,4 +14,3 @@ def mix_color(color_1: str, color_2: str) -> str:
 if __name__ == '__main__':
     color_1, color_2 = input(), input()
     mix_color(color_1, color_2)
-
