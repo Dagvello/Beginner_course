aa341be4a9a1bd1a27b31343e07328c561a8bed6
Dagvello <dagvello@ukr.net>
2023-02-20 14:46:28 +0200

    Исправил текст сообщения

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -31,25 +31,25 @@ class CalculatorTest(TestCase):
         """Функция проверки наличие более одного знака"""
         with self.assertRaises(ValueError) as e:
             calculator('2+2+2')
-        self.assertEqual('Выражение должно содержать два целых числа и знак (+-*/)', e.exception.args[0])
+        self.assertEqual('Выражение должно содержать 2 целых числа и один знак', e.exception.args[0])
 
     def test_many_signs(self):
         """Функция проверки наличие разных знаков"""
         with self.assertRaises(ValueError) as e:
             calculator('2+3*10')
-        self.assertEqual('Выражение должно содержать два целых числа и знак (+-*/)', e.exception.args[0])
+        self.assertEqual('Выражение должно содержать 2 целых числа и один знак', e.exception.args[0])
 
     def test_no_ints(self):
         """Функция проверки, что число целое"""
         with self.assertRaises(ValueError) as e:
             calculator('2.0+3.0')
-        self.assertEqual('Выражение должно содержать два целых числа и знак (+-*/)', e.exception.args[0])
+        self.assertEqual('Выражение должно содержать 2 целых числа и один знак', e.exception.args[0])
 
     def test_strings(self):
         """Функция проверки, что число целое"""
         with self.assertRaises(ValueError) as e:
             calculator('a+b')
-        self.assertEqual('Выражение должно содержать два целых числа и знак (+-*/)', e.exception.args[0])
+        self.assertEqual('Выражение должно содержать 2 целых числа и один знак', e.exception.args[0])
 
 
 if __name__ == '__main__':
