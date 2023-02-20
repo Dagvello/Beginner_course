777a1b5df46dc0e4dfb5261f48672cc4edf7f544
Dagvello <dagvello@ukr.net>
2023-02-20 13:56:55 +0200

    Добавил тестовые функции

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,9 +1,15 @@
 from unittest import TestCase, main
-from Beginner_course.calculator import calculator
+from calculator import calculator
+
 
 class CalculatorTest(TestCase):
     def test_plus(self):
-        self.assertEqual(calculator('2+2', 4))
-
+        self.assertEqual(calculator('2+2'), 4)
+    def test_minus(self):
+        self.assertEqual(calculator('5-3'), 2)
+    def test_multi(self):
+        self.assertEqual(calculator('5*3'), 15)
+    def test_divide(self):
+        self.assertEqual(calculator('10/5'), 2.0)
 if __name__ == '__main__':
     main()
