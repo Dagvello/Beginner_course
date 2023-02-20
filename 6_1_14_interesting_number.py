3c72a6d4a606dedcd6f3f4da831f5a32b3e3be66
Dagvello <dagvello@ukr.net>
2023-02-21 00:25:52 +0200

    Использовал ChatGPT для улучшения кода

--- 6_1_14_interesting_number.py
+++ 6_1_14_interesting_number.py
@@ -1,15 +1,24 @@
-def interesting_number(number: list) -> None:
+def is_interesting_number(number_list: list[int]) -> bool:
     """ Функция определяет интересное число.
-        Число интересное, если в нем разность максимальной и минимальной цифры
-        равняется средней по величине цифре.
+        Число интересное, если в нем разность максимальной и минимальной цифры равняется средней по величине цифре
     """
-    num_list = sorted(number)
-    if int(num_list[2]) - int(num_list[0]) == int(num_list[1]):
-        print('Число интересное')
-    else:
-        print('Число неинтересное')
+    if len(number_list) != 3:
+        raise ValueError("Неверное количество цифр в числе")
+    if not all(isinstance(n, int) and 0 <= n <= 9 for n in number_list):
+        raise ValueError("Неверный формат входных данных")
+    num_list = sorted(number_list)
+    return int(num_list[2]) - int(num_list[0]) == int(num_list[1])
 
 
 if __name__ == '__main__':
-    number = list(input())
-    interesting_number(number)
+    number_str = input("Введите список цифр через запятую: ")
+    number_list = list(map(int, number_str.split(",")))
+    try:
+        is_interesting = is_interesting_number(number_list)
+        if is_interesting:
+            print('Число интересное')
+        else:
+            print('Число неинтересное')
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+
