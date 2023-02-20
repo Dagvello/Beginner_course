7b575bca35e8f1409b93214c7f132db069f1a32b
Dagvello <dagvello@ukr.net>
2023-02-21 00:09:12 +0200

    init

---
+++ 6_1_14_interesting_number.py
@@ -0,0 +1,14 @@
+def interesting_number(number: list) -> None:
+    """ Функция определяет интересное число.
+        Число интересное, если в нем разность максимальной и минимальной цифры равняется средней по величине цифре
+    """
+    num_list = sorted(number)
+    if int(num_list[2]) - int(num_list[0]) == int(num_list[1]):
+        return print('Число интересное')
+    else:
+        return print('Число неинтересное')
+
+
+if __name__ == '__main__':
+    number = list(input())
+    interesting_number(number)
