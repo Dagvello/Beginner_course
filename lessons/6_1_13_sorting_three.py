dfcea0f7cc46ad9271e3396e226a354f82676d28
Dagvello <dagvello@ukr.net>
2023-02-20 22:58:03 +0200

    init

---
+++ lessons/6_1_13_sorting_three.py
@@ -0,0 +1,10 @@
+def sorting_three(my_list: list) -> list:
+    """Функция выводит сортированный список"""
+    return print(sorted(my_list), end='\n')
+
+
+if __name__ == '__main__':
+    my_list = []
+    for i in range(3):
+        my_list.append(int(input()))
+    sorting_three(my_list)
