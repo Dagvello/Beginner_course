4096dd01f8e37be66dbf4b4d6cff17e3609454c0
Dagvello <dagvello@ukr.net>
2023-02-21 09:10:36 +0200

    после ChatGPT

--- 6_1_15_abs_summa.py
+++ 6_1_15_abs_summa.py
@@ -1,8 +1,23 @@
-def abs_summa(list: list[float]) -> float:
-    """Функция суммирует все абсолютные значения списка"""
-    return sum(abs(x) for x in list)
+def abs_sum(lst: list[float]) -> float:
+    """
+    Функция суммирует все абсолютные значения списка.
+
+    Args:
+        lst: Список чисел.
+
+    Returns:
+        Сумма абсолютных значений списка.
+
+    Raises:
+        TypeError: Если элементы списка не могут быть приведены к типу float.
+
+    """
+    return sum(map(abs, lst))
 
 
 if __name__ == '__main__':
-    my_list = [abs(float(input())) for _ in range(5)]
-    print(abs_summa(my_list))
+    try:
+        my_list = [float(input()) for _ in range(5)]
+        print(abs_sum(my_list))
+    except ValueError:
+        print("Ошибка ввода. Проверьте правильность введенных значений.")
