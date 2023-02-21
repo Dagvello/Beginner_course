3e46c6e73e68708beec4b44dbf4579eeff570be9
Dagvello <dagvello@ukr.net>
2023-02-21 23:52:28 +0200

    Разделил функции проверки, добавид docstring

--- lessons/Knight_move.py
+++ lessons/Knight_move.py
@@ -1,4 +1,5 @@
-def is_valid_knight_move(move):
+def examination(move):
+    """Проверка правильности входных данных"""
     valid_squares = [f + r for r in '12345678' for f in 'abcdefgh']
     move = move.lower()
     if len(move) != 5:
@@ -7,6 +8,10 @@ def is_valid_knight_move(move):
         return False
     if move[2] != '-':
         return False
+    return True
+
+def is_valid_knight_move(move):
+    """Проверка правильности хода"""
     file_diff = abs(ord(move[0]) - ord(move[3]))
     rank_diff = abs(int(move[1]) - int(move[4]))
     if file_diff == 1 and rank_diff == 2:
@@ -19,8 +24,10 @@ def is_valid_knight_move(move):
 if __name__ == '__main__':
     # Пример использования функции is_valid_knight_move
     move = input()
-    if is_valid_knight_move(move):
-        print('YES')
+    if examination(move):
+        if is_valid_knight_move(move):
+            print('YES')
+        else:
+            print('NO')
     else:
-        print('NO')
-
+        print('ERROR')
