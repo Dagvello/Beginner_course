762baad1efe475c5ca88518d791158f4f5570adc
Dagvello <dagvello@ukr.net>
2023-02-22 23:50:36 +0200

    init

--- lessons/temp_file.py
+++ lessons/temp_file.py
@@ -1,11 +1,8 @@
-from math import sqrt
-
-
-def euclidean_distance(x1: float, y1: float, x2: float, y2: float) -> float:
-    """Функция вычисляет эвклидовое расстояние"""
-    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-
-
-if __name__ == '__main__':
-    x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
-    print(euclidean_distance(x1, y1, x2, y2))
+m = int(input())
+n = int(input())
+if n < m:
+    for i in range(m, n-1, -1):
+        print(i)
+else:
+    for i in range(m, n+1):
+        print(i)
