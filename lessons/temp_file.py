0ef63899a791dd1f07ae3ca6eb78534c30d83d03
Dagvello <dagvello@ukr.net>
2023-02-28 22:53:32 +0200

    init

--- lessons/temp_file.py
+++ lessons/temp_file.py
@@ -1,6 +1,5 @@
-from math import log
-n = int(input())
-total = 1
-for i in range(2, n + 1):
-    total += 1 / i
-print(total - log(n))
+number = int(input())
+total = 0
+for n in range(1, number + 1):
+    total += n * (-1) ** (n + 1)
+print(total)
