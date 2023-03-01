25502b2a7d546d73c387ad3a3e8d733b1904e216
Dagvello <dagvello@ukr.net>
2023-03-01 15:49:55 +0200

    init

--- lessons/temp_file.py
+++ lessons/temp_file.py
@@ -1,5 +1,15 @@
-number = int(input())
-total = 0
-for n in range(1, number + 1):
-    total += n * (-1) ** (n + 1)
-print(total)
+n = int(input())
+maximum1 = 0
+maximum2 = 0
+for i in range(n):
+    number = int(input())
+    if number > maximum1:
+        maximum2 = maximum1
+        maximum1 = number
+    elif number > maximum2:
+        maximum2 = number
+print(maximum1)
+print(maximum2)
+
+
+
