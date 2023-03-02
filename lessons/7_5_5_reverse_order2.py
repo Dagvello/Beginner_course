94410124d3870a3e6ae0e62bdc6c55f9db940b9a
Dagvello <dagvello@ukr.net>
2023-03-02 15:12:13 +0200

    init

---
+++ lessons/7_5_5_reverse_order2.py
@@ -0,0 +1,10 @@
+def reverse_order(number: int) -> int:
+    """Функция преобразует натуральное число в число из цифр в обратном порядке"""
+    while number > 0:
+        num = number % 10
+        print(num, end='')
+        number = number//10
+
+
+if __name__ == '__main__':
+    reverse_order(int(input()))
