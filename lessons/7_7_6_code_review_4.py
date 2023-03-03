d4287f44da875305eec8b96d52472c5041c86242
Dagvello <dagvello@ukr.net>
2023-03-03 19:38:41 +0200

    1 ошибка: изменил знак

--- lessons/7_7_6_code_review_4.py
+++ lessons/7_7_6_code_review_4.py
@@ -3,7 +3,7 @@ max_digit = n % 10
 while n > 0:
     digit = n % 10
     if digit % 3 == 0:
-        if digit < max_digit:
+        if digit > max_digit:
             digit = max_digit
     n = n % 10
 if max_digit == 0:
