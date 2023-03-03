3112921c86f3893bf939b0f569c32b3d82d960cb
Dagvello <dagvello@ukr.net>
2023-03-03 11:33:07 +0200

    init

--- lessons/7_6_8_follow_the_rules.py
+++ lessons/7_6_8_follow_the_rules.py
@@ -1,6 +1,10 @@
 def follow_the_rules(number: int) -> int:
-    pass
+    """Функция выводит числа от 1 до n включительно за исключением"""
+    for i in range(1, number + 1):
+        if 5 <= i <= 9 or 17 <= i <= 37 or 78 <= i <= 87:
+            continue
+        print(i)
 
 
 if __name__ == '__main__':
-    pass
+    follow_the_rules(int(input()))
